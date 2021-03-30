3586fd648ef46966d6fd758e14b3779908ec358a
gympohnpimol <gympohnpimol@gmail.com>
2021-03-30 19:18:24 +0700

    angular

---
+++ practice-angular/src/app/counting/counting.component.ts
@@ -0,0 +1,7 @@
+import { Component } from'@angular/core';
+@Component({
+    selector:'app-counting',
+    templateUrl:'./counting.component.html'
+})export class CountingComponent {
+    constructor() {}
+}
