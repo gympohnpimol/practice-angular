9af5a16c51e8066549291e0e970dca33e00982dc
gympohnpimol <gympohnpimol@gmail.com>
2021-04-01 00:39:10 +0700

    Lab3 Component Template CSS

--- practice-angular/src/app/counting/counting.component.ts
+++ practice-angular/src/app/counting/counting.component.ts
@@ -1,7 +1,8 @@
 import { Component } from'@angular/core';
 @Component({
     selector:'app-counting',
-    templateUrl:'./counting.component.html'
+    templateUrl:'./counting.component.html',
+    styleUrls: ['./counting.component.css']
 })export class CountingComponent {
     constructor() {}
 }
