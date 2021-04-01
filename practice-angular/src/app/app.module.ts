06b7e7998e41a66a9a0e5e32f84c6bedb78600fe
gympohnpimol <gympohnpimol@gmail.com>
2021-04-01 19:50:01 +0700

    Homework1

--- practice-angular/src/app/app.module.ts
+++ practice-angular/src/app/app.module.ts
@@ -4,20 +4,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component'
 import { CountingComponent } from './counting/counting.component'
-import { FormsModule } from '@angular/forms';
-import { LabComponent } from './lab4/lab4.component'
+import { ReactiveFormsModule, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { LabComponent } from './lab4/lab4.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CountingComponent,
-    LabComponent
+    LabComponent,
+    SideNavComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatMenuModule,
+    LayoutModule,
+    MatButtonModule,
+    MatListModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
